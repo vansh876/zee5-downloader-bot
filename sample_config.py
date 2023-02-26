2f223b8b0c4252305785b8b11f0ba35fe2d2c618
Etchial <98379633+Etchial@users.noreply.github.com>
2023-02-26 14:06:44 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6036876137:AAGppgQFPIv1aLHTsZir74veDWjZSFYFciQ")
 
     # The Telegram API things
     # Get these values from my.telegram.org
@@ -11,7 +11,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5888679747").split())
 
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
@@ -29,5 +29,5 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://fp8130:fp8130@cluster0.sxpae5r.mongodb.net/?retryWrites=true&w=majority")
     
